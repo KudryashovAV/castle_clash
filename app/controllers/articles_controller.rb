709fa93ec078a42b420dbee0b4313426fa4fffa1
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2017-02-21 10:33:50 +0300

    Add prepare video url

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,9 +16,32 @@ class ArticlesController < ApplicationController
     redirect_to articles_url
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    debugger
+    @article = Article.find(params[:id])
+    url = prepare_video_url(article_params[:video_link])
+    article_params.merge!(video_link: url)
+    @article.update_attributes(article_params)
+    redirect_to articles_url
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_url
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:body, :video_link)
+  end
+
+  def prepare_video_url(url)
+    url.gsub!("www.youtube.com/watch?v=", "youtube.com/embed/")
   end
 end
